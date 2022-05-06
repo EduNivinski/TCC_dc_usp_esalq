494512b7feb65556a892f87eed4db624745e2f35
EduNivinski <88976645+EduNivinski@users.noreply.github.com>
2022-05-06 20:57:41 -0300

    Atualização
    adição de R-project
    edição do TCC (materiais e métodos)
    nova base de dados 2021 contendo todos os dados de interesse dos anos anteriores

---
+++ data/r-project_tcc/tcc_data.R
@@ -0,0 +1,64 @@
+
+# Passo a passo do desenvolvimento das análises de dados
+
+# link de auxílio: "https://github.com/rstudio/cheatsheets/blob/main/data-transformation.pdf"
+
+# instalação dos pacotes
+install.packages("tidyverse")
+
+library("tidyverse")
+library(readxl)
+
+# renomeando as variáveis de interesse
+whr_all_data21 <- read_excel("DataPanelWHR2021C2.xls")
+db_all <- whr_all_data21
+
+db_all <- db_all %>% rename(country = 1,
+                         year = 2,
+                         ladder_score = 3,
+                         log_gdp = 4,
+                         social_support = 5,
+                         healthy_exp = 6)
+
+# para visualização dos dados (total de 165 países)
+table(db_all$year)
+unique(db_all$country)
+
+
+#excluindo as linhas que possuem anos que não são de interesse (todas exceto 2019 e 2020 )
+db_all <- db_all[!(db_all$year==2005),]
+db_all <- db_all[!(db_all$year==2006),]
+db_all <- db_all[!(db_all$year==2007),]
+db_all <- db_all[!(db_all$year==2008),]
+db_all <- db_all[!(db_all$year==2009),]
+db_all <- db_all[!(db_all$year==2010),]
+db_all <- db_all[!(db_all$year==2011),]
+db_all <- db_all[!(db_all$year==2012),]
+db_all <- db_all[!(db_all$year==2013),]
+db_all <- db_all[!(db_all$year==2014),]
+db_all <- db_all[!(db_all$year==2015),]
+db_all <- db_all[!(db_all$year==2016),]
+db_all <- db_all[!(db_all$year==2017),]
+db_all <- db_all[!(db_all$year==2018),]
+db_all
+
+# excluindo linhas que possuem dados ausentes
+db_all <- na.omit(db_all)
+db_all
+
+# excluindo paises que não estão em ambos os anos
+table(db_all$country==1)
+
+db_all <- subset(db_all,duplicated(country) | duplicated(country, fromLast=TRUE))
+db_all
+
+# quantidade de países restantes (76) 
+unique(db_all$country)
+
+# excluindo colunas desnecessárias
+db_all <- db_all[-(5)]
+db_all <- db_all[-(6:10)]
+
+test_db <- db_all
+test_db
+
